2a3c7cda787e0d94bb8f5630c474ec564c563ae5
safrizal <safrizalaji16@gmail.com>
2022-06-29 21:08:29 +0700

    feat: add product filter component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Banner from "./components/home-banner";
 import ProductCard from "./components/product-card";
 import AboutPage from "./pages/about";
 import Newslettersignup from "./components/newsletter-signup";
-
+import ProductFilter from "./components/product-filter";
 // import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      <Newslettersignup />
+      <ProductFilter />
+      {/* <Newslettersignup /> */}
       {/* <Banner />
       <ProductCard />
       <AboutPage /> */}
