b0ed4e4efb8f1993809d08697dac5217d8a92e9a
kibuelll <kibuellyoi34@gmail.com>
2022-07-20 23:55:38 +0700

    responsive-homepage :main footer and changing assets in homepage to GLOD

--- src/components/main-footer/index.js
+++ src/components/main-footer/index.js
@@ -1,45 +1,45 @@
 import { Link } from "react-router-dom";
-import { Row, Col, Typography} from "antd";
+import { Row, Col, Typography } from "antd";
 import {
   FacebookOutlined,
   TwitterOutlined,
   InstagramOutlined,
   YoutubeOutlined,
 } from "@ant-design/icons";
-import { Footer } from "antd/lib/layout/layout";
+import "./index.css"
+import logoFooter1 from "../../assets/GLOD WEB/LOGO GLOD MONOCR B.jpg"
+import logoFooter2 from "../../assets/GLOD WEB/LOGO GLOD LATN B.jpg"
+
 
 const { Title } = Typography;
 
 export default function MainFooter() {
   return (
-    <Row>
-      <Col xs={{ span: 12 }} xl={{ span: 12 }}>
-        <img src="" alt="failed load image">
-        </img>
-        <Title
-          style={{
-            width: "200px",
-            textAlign: "center",
-          }}
-        >
-          Ant Design ©2018 Created by Ant UED
+    <Row style={{ backgroundColor: "black", color: 'white' }}>
+      <Col id="logofooter" xs={{ span: 12 }} xl={{ span: 8 }} style={{textAlign:'center'}}>
+        <img src={logoFooter1} alt="" style={{width:'50px', height:'50px'}}/>
+        <img src={logoFooter2} alt="" style={{width:'50px', height:'50px'}}/>
+      </Col>
+      <Col xs={{ span: 12 }} xl={{ span: 8 }} style={{textAlign:'center'}}>
+        <Title id="copyright" level={5} style={{ color: "white",padding:'10px 0px',fontSize:'15px' }}>
+          GLOD Sportwear ©2022 Created by GLOD
         </Title>
       </Col>
-      <Col>
-        <Title level={3}>Social</Title>
-        <Row style={{ fontSize: "30px" }}>
-          <Link to="/">
-            <FacebookOutlined style={{ marginRight: '10px' }} />
-          </Link>
-          <Link to="/">
-            <TwitterOutlined style={{ marginRight: '10px' }} />
-          </Link>
-          <Link to="/">
-            <InstagramOutlined style={{ marginRight: '10px' }} />
-          </Link>
-          <Link to="/">
-            <YoutubeOutlined style={{ marginRight: '10px' }} />
-          </Link>
+      <Col id="social" xs={{ span: 24 }} xl={{ span: 8 }} style={{textAlign:'center'}} >
+        <Row  style={{ fontSize: "30px", color: "white" }} justify="center">
+          <Title level={5} style={{ color: "white",padding:'10px 0px' }}>Social</Title>
+            <Link to="/">
+              <FacebookOutlined style={{ margin: ' 0px 20px', color: "white",fontSize:'25px' }} />
+            </Link>
+            <Link to="/">
+              <TwitterOutlined style={{ margin: ' 0px 0px', color: "white",fontSize:'25px' }} />
+            </Link>
+            <Link to="/">
+              <InstagramOutlined style={{ margin: ' 0px 10px', color: "white",fontSize:'25px' }} />
+            </Link>
+            <Link to="/">
+              <YoutubeOutlined style={{ margin: ' 0px 10px', color: "white",fontSize:'25px' }} />
+            </Link>
         </Row>
         {/* tanda tangan */}
       </Col>
