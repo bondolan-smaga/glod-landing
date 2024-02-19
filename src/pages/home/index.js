6f52ae455658a6d377773e5f4f6edd9066ddf048
safrizal <safrizalaji16@gmail.com>
2024-02-19 11:18:22 +0700

    feat: change pic

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -18,7 +18,7 @@ export default function HomePage() {
           <Banner
             style={{ margin: "0 0" }}
             background={
-              "url(https://www.hummel.net/dw/image/v2/BDWL_PRD/on/demandware.static/-/Library-Sites-hummel-shared/default/dw184bfe8a/homepage/UDSALG%20OG%20KAMPAGNER/1480x650_Hero_SALE_SS22_1.jpg)"
+              "url(https://www.hummel.net/dw/image/v2/BDWL_PRD/on/demandware.static/-/Library-Sites-hummel-shared/default/dw68b5796f/homepage/1480x650_SPORT_FOOTBALL_1.0.jpg)"
             }
           />
         </Col>
@@ -34,7 +34,7 @@ export default function HomePage() {
           <Banner
             style={{ margin: "30px 0" }}
             background={
-              "url(https://www.hummel.net/dw/image/v2/BDWL_PRD/on/demandware.static/-/Library-Sites-hummel-shared/default/dwd1379773/homepage/STYLE/1480x650_Hero_Training_4.0.jpg)"
+              "url(https://www.hummel.net/dw/image/v2/BDWL_PRD/on/demandware.static/-/Library-Sites-hummel-shared/default/dw68b5796f/homepage/1480x650_SPORT_FOOTBALL_1.0.jpg)"
             }
           />
         </Col>
@@ -44,7 +44,7 @@ export default function HomePage() {
               <Banner
                 style={{ margin: "20px 0" }}
                 background={
-                  "url(https://www.hummel.net/on/demandware.static/-/Library-Sites-hummel-shared/default/dw6c9591c7/homepage/STYLE/720x600_Split_Legacy_Core_V2_2.0.jpg)"
+                  "url(https://www.hummel.net/dw/image/v2/BDWL_PRD/on/demandware.static/-/Library-Sites-hummel-shared/default/dw68b5796f/homepage/1480x650_SPORT_FOOTBALL_1.0.jpg)"
                 }
               />
             </Col>
@@ -52,7 +52,7 @@ export default function HomePage() {
               <Banner
                 style={{ margin: "20px 0" }}
                 background={
-                  "url(https://www.hummel.net/on/demandware.static/-/Library-Sites-hummel-shared/default/dwecfc136d/homepage/STYLE/720x600_Split_FTW_3.1.jpg)"
+                  "url(https://www.hummel.net/dw/image/v2/BDWL_PRD/on/demandware.static/-/Library-Sites-hummel-shared/default/dw68b5796f/homepage/1480x650_SPORT_FOOTBALL_1.0.jpg)"
                 }
               />
             </Col>
